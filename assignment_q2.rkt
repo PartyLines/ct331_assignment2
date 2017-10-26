ceeca644a00443eadd9ea8554f02f82b19fb26a0
MARK AARON VINCENT MICHAEL HARRINGTON <15488702@IT.NUIGALWAY.IE>
2017-10-26 15:28:37 +0100

    Frodo codo

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,15 +1,5 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
 
 (provide ins_beg)
 
@@ -20,25 +10,21 @@
 (define (ins_end el lst)
   (append lst (cons el empty)) )
 ;C
-(define (cout_top_level lst)
-  (if (empty? lst) 0 (+ 1 (cout_top_level (cdr lst)))) )
+(define (count_top_level lst)
+  (if (empty? lst) 0 (+ 1 (count_top_level (cdr lst)))) )
 ;D
-(define (count_instances lst)
-  (cond [(empty? lst) 0]
-        [(list? (car lst)) (+ (count_instances (cadar lst)) (count_instances (cdr lst)))]
-        [else (+ (count_instances (cdr lst)) 1)]
-        )
-  )
+(define (count_instances el lst)
+  (if (empty? lst) 0 
+  (+ (if (= el (car lst)) 1 0) (count_instances el (cdr lst)))
+  ))
 ;E
-(define (count_instances_tr lst)
-  (citr_func lst 0)
+(define (count_instances_tr el lst)
+  (citr_func el lst 0)
   )
-(define (citr_func lst val)
-  (cond [(empty? lst) val]
-        [(list? (car lst)) (citr_func (append (cadar lst) (cdr lst)) val)]
-        [else (citr_func (cdr lst) (+ val 1))]
-        )
-  )
+(define (citr_func el lst val)
+  (if (empty? lst) val 
+  (citr_func el (cdr lst) (+ (if (= el (car lst)) 1 0) val))
+  ))
 ;F
 (define (count_instances_deep lst comp)
   (cid_func lst 0 comp)
